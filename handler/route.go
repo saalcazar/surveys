355e814d75f793dc5a2efb243f8e1675b5b2242a
saalcazar <alcazar.samuel@gmail.com>
2024-07-02 00:28:02 -0400

    Routing modification

--- handler/route.go
+++ handler/route.go
@@ -4,6 +4,6 @@ import "net/http"
 
 func RouteSurvey(mux *http.ServeMux, storageSurvey StorageSurvey) {
 	h := NewServiceSurvey(storageSurvey)
-	mux.HandleFunc("v1/survey/create", h.create)
+	mux.HandleFunc("/v1/survey/create", h.create)
 	// mux.HandleFunc("v1/survey/getall", h.getAll)
 }
